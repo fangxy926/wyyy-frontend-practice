93bd104fb4da49cb170ac085458a5353a4e4a360
fangxy926 <1515243746@qq.com>
2023-04-27 15:46:47 +0800

    eslint check

--- pages/userinfo.vue
+++ pages/userinfo.vue
@@ -9,8 +9,16 @@
       </van-field> -->
       <van-field class="avatar-field" label="头像" right-icon="" readonly>
         <template #right-icon>
-          <van-uploader v-model="avatarList" class="avatar-uploader" :after-read="afterRead" :before-read="beforeRead"
-            :max-count="1" preview-size="60px" :preview-full-image="false" accept="image/*" />
+          <van-uploader
+            v-model="avatarList"
+            class="avatar-uploader"
+            :after-read="afterRead"
+            :before-read="beforeRead"
+            :max-count="1"
+            preview-size="60px"
+            :preview-full-image="false"
+            accept="image/*"
+          />
         </template>
       </van-field>
       <van-field v-model="formData.name" name="name" label="姓名" placeholder="请输入姓名" :rules="rules.name" />
